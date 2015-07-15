ab302de971a9a65915077e7f8172065e91bc7d4c
Igor Pavlov <nwlunatic@yandex.ru>
2015-07-15 17:22:31 +0600

    add windows start tasks

--- windows-cloudinit.ps1
+++ windows-cloudinit.ps1
@@ -1,4 +1,5 @@
 #ps1_sysnative
+$ErrorActionPreference = 'Stop'
 
 $JAVA_VERSION = "8u45"
 $AGENT = "agent.exe"
@@ -10,34 +11,37 @@ $JAVA_URL = "$STORAGE/windows/java/jre-$JAVA_VERSION-windows-x64.exe"
 $SELENIUM_URL = "$STORAGE/$SELENIUM_SERVER_STANDALONE"
 $AGENT_URL = "$STORAGE/windows/agent.exe"
 
-Write-Output "User:"
-[Environment]::UserName
+$DIRECTORY = "C:\\"
+$USERNAME = "test"
 
 Write-Output "------------------software--------------------"
-
-cd C:\
 $wc = New-Object System.Net.WebClient
 
 # install oracle java
-$wc.Downloadfile($JAVA_URL, "$JAVA")
-Start-Process -FilePath "$JAVA" -passthru -wait -ArgumentList "/s INSTALLDIR=c:\java /L install64.log"
+$wc.Downloadfile($JAVA_URL, "$DIRECTORY\$JAVA")
+Start-Process -FilePath "$DIRECTORY\$JAVA" -passthru -wait -ArgumentList "/s INSTALLDIR=c:\java /L install64.log"
 
 # download selenium-server
-$wc.Downloadfile($SELENIUM_URL, "$SELENIUM_SERVER_STANDALONE")
+$wc.Downloadfile($SELENIUM_URL, "$DIRECTORY\$SELENIUM_SERVER_STANDALONE")
 
 # download vmmaster-agent
-$wc.DownloadFile($AGENT_URL, "$AGENT")
-
-Write-Output "--------------credential user-----------------"
-$username = "test"
-$password = ConvertTo-SecureString -String "123456" -AsPlainText -Force
-$user = [ADSI]'WinNT://./test'
-$user.SetPassword("123456")
-$cred = New-Object System.Management.Automation.PSCredential $username,$password
+$wc.DownloadFile($AGENT_URL, "$DIRECTORY\$AGENT")
 
 Write-Output "-------------switch to desktop----------------"
 (New-Object -ComObject shell.application).toggleDesktop()
 
 Write-Output "---------------start software-----------------"
-Start-Process $AGENT -Credential $cred
-Start-Process java -ArgumentList "-jar $SELENIUM_SERVER_STANDALONE"  -Credential $cred
+
+$machine = [Environment]::MachineName
+$user = "$machine\$USERNAME"
+
+$agent_action = New-ScheduledTaskAction "$DIRECTORY\$AGENT"
+$agent_task = "agent"
+Register-ScheduledTask -TaskName $agent_task -User $user -Action $agent_action
+
+$selenium_server_action = New-ScheduledTaskAction -Execute "java" -Argument "-jar $DIRECTORY\$SELENIUM_SERVER_STANDALONE -port 4445"
+$selenium_server_task = "selenium_server"
+Register-ScheduledTask -TaskName $selenium_server_task -User $user -Action $selenium_server_action
+
+Start-ScheduledTask $agent_task
+Start-ScheduledTask $selenium_server_task
