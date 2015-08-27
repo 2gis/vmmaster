7daab3bbf59bb7ba66bfe21c00b44dadfba08553
Igor Pavlov <i.pavlov@2gis.ru>
2015-08-27 16:33:08 +0600

    testing libvirt from source

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,9 +3,15 @@
 # install dependencies
 sudo apt-get update
 
+# enable cloud-archive to get the latest libvirt
+sudo apt-get -y install python-software-properties
+sudo add-apt-repository -y cloud-archive:icehouse
+
 # install kvm with libvirt
-sudo apt-get -y install qemu-kvm libvirt-bin 
+sudo apt-get -y install qemu-kvm libvirt-bin
+
 # install libvirt-dev for python bindings
+sudo apt-get install libvirt-dev
 sudo apt-get -y install libvirt-dev
 
 # install openstack dependencies
