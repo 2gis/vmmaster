215ae805c02b2ab87324682510a15f6b0e3694ec
Igor Pavlov <i.pavlov@2gis.ru>
2015-08-27 15:37:24 +0600

    check libvirt version

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,4 +2,12 @@
 
 # install dependencies
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev libffi-dev virtinst libvirt-bin libvirt-dev kvm
+
+# install kvm with libvirt
+sudo apt-get -y install qemu-kvm libvirt-bin 
+# install libvirt-dev for python bindings
+sudo apt-get -y install libvirt-dev
+
+# install openstack dependencies
+sudo apt-get -y install libssl-dev libffi-dev
+#sudo apt-get install build-essential  virtinst libvirt-bin libvirt-dev kvm
