4096ae6526a13b55ea689b61502912f4cf5e67bb
Igor Pavlov <i.pavlov@2gis.ru>
2015-08-27 11:54:07 +0600

    refactor travis ci config

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# install dependencies
+# enable cloud-archive to get the latest libvirt
+sudo apt-get -y install python-software-properties
+sudo add-apt-repository -y cloud-archive:icehouse
+
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev libffi-dev virtinst libvirt-bin libvirt-dev kvm
+
+# install kvm with libvirt
+sudo apt-get -y install qemu-kvm libvirt-bin
+
+# install libvirt dependencies
+sudo apt-get -y install libvirt-dev
+
+# install openstack dependencies
+sudo apt-get -y install libssl-dev libffi-dev
