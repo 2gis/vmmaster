79f4e6a6ed75c19f888219c1eaa415dfb42226b1
Stanislav Zubov <z00sts@users.noreply.github.com>
2017-10-09 12:21:55 +0700

    remove-get-node-info (#256)

--- vmmaster/api/__init__.py
+++ vmmaster/api/__init__.py
@@ -31,7 +31,6 @@ def version():
 @api.route('/status')
 def status():
     return render_json({
-        'node': helpers.get_node_info(),
         'sessions': helpers.get_sessions(),
         'queue': helpers.get_queue(),
         'platforms': vmpool_helpers.get_platforms(),
